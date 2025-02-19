cd8099b0a400344712806b1b58430e8c19178c4e
Achim <achim@zibra.de>
2025-02-19 20:54:20 +0100

    first draft version of a prediction service

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a simple prediction service for [moondream](https://github.com/vikhyat/moondream) which creates captions for given images either in short or long length.
+
+## Prerequites
+[cog](https://cog.run/getting-started/): which is a build tool to create docker images from `cog.yaml` definition
+
+## Run
---
+++ predict.py
@@ -0,0 +1,44 @@
+from cog import BasePredictor, Input, Path
+
+import moondream as md
+from PIL import Image
+
+class Predictor(BasePredictor):
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        # self.model = torch.load("./weights.pth")
+        # Initialize with local model path. Can also read .mf.gz files, but we recommend decompressing
+        # up-front to avoid decompression overhead every time the model is initialized.
+        self.model = md.vl(model="./model/moondream-2b-int8.mf")
+
+    # The arguments and types the model takes as input
+    def predict(self,
+          image: Path = Input(description="An input image"),
+          length: str = Input(description="Caption length either 'short' or 'long'. default is 'long'")
+    ) -> str:
+        """Run a single prediction on the model"""
+        # Load and process image
+        # image = Image.open("path/to/image.jpg")
+        encoded_image = model.encode_image(image)
+
+        return self.model.caption(encoded_image)["caption"]
+
+        # processed_image = preprocess(image)
+        # output = self.model(processed_image)
+        # return postprocess(output)
+
+# # Initialize with local model path. Can also read .mf.gz files, but we recommend decompressing
+# # up-front to avoid decompression overhead every time the model is initialized.
+# model = md.vl(model="path/to/moondream-2b-int8.mf")
+
+# # Load and process image
+# image = Image.open("path/to/image.jpg")
+# encoded_image = model.encode_image(image)
+
+# # Generate caption
+# caption = model.caption(encoded_image)["caption"]
+# print("Caption:", caption)
+
+# # Ask questions
+# answer = model.query(encoded_image, "What's in this image?")["answer"]
+# print("Answer:", answer)
